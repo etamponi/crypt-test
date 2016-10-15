eaa860a9be431165665bbcd8b285da3f347ab47f
Emanuele Tamponi <etamponi@fogcreek.com>
2016-10-15 16:28:46 +0200

    Fix fix-rotation script

--- fix-rotation.sh
+++ fix-rotation.sh
@@ -13,6 +13,6 @@ rm -r .git/git-crypt
 git mv .gitattributes .gitattributes.tmp
 git commit -m "Moved .gitattributes to fix key rotation"
 git pull --no-edit
+git crypt unlock ${new_key_file}
 git mv .gitattributes.tmp .gitattributes
 git commit -m "Readded .gitattributes to fix key rotation"
-git crypt unlock ${new_key_file}
