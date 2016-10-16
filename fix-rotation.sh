691cc04bd106824029e6139d7f4d434fb5a77e7c
Emanuele Tamponi <etamponi@fogcreek.com>
2016-10-16 21:53:59 +0200

    Fix fix-rotation

--- fix-rotation.sh
+++ fix-rotation.sh
@@ -11,58 +11,22 @@ then
     exit 1
 fi
 
-checkout_encrypted() {
-    encrypted_files=$(git crypt status | grep -v "not encrypted" | awk '{ print $2; }')
-    for f in ${encrypted_files}
-    do
-        git checkout $f
-    done
-}
-
-# Get encrypted files before we remove .gitattributes, that's why we don't use checkout_encrypted
+# Get encrypted files
 encrypted_files=$(git crypt status | grep -v "not encrypted" | awk '{ print $2; }')
-
-# Forget that we decrypted the repo
-rm -rf .git/git-crypt
-
-# Forget about encrypted files
-git mv .gitattributes .gitattributes.tmp
-git commit -m "$(hostname) Moved .gitattributes to fix key rotation"
-
-# Revert any local dirtiness that git pull has caused
+# Revert any local dirtiness that git pull has caused on the encrypted files
 for f in ${encrypted_files}
 do
     git checkout $f
 done
+
+# Lock repository and forget key
+git crypt lock
+rm -rf .git/git-crypt
 
 # Stash any local change caused by git pull or by our local work
 stash_out=$(git stash)
 echo ${stash_out}
 
-# Pull again and merge the previous commit
-git pull --no-edit
-# Revert any local dirtiness that git pull has caused
-for f in ${encrypted_files}
-do
-    git checkout $f
-done
-
-git crypt unlock ${new_key_file}
-
-# Revert the .gitattributes change
-git mv .gitattributes.tmp .gitattributes
-git commit -m "$(hostname) Readded .gitattributes to fix key rotation"
-
-# Make sure we don't have any dirty encrypted file
-checkout_encrypted
-
-# Pop from stash
-if [ "${stash_out}" != "No local changes to save" ]
-then
-	git stash pop
-	# Check again because the last pop may have changed the .gitattributes
-	checkout_encrypted
-fi
-
-rm -rf .git/git-crypt
+# Pull again and unlock with new key file
+git pull
 git crypt unlock ${new_key_file}
